5e2286c698abaf32ed33f5c24e89902a093edf50
Karsa Wang <wanghongtao114@gamil.com>
2017-05-27 17:21:13 +0800

    support carthage build

---
+++ MLeakFinder/MLeakFinder.h
@@ -0,0 +1,19 @@
+//
+//  MLeakFinder.h
+//  MLeakFinder
+//
+//  Created by karsa on 2017/5/27.
+//  Copyright © 2017年 zeposhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MLeakFinder.
+FOUNDATION_EXPORT double MLeakFinderVersionNumber;
+
+//! Project version string for MLeakFinder.
+FOUNDATION_EXPORT const unsigned char MLeakFinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLeakFinder/PublicHeader.h>
+
+
